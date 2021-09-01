a534a27ea2723cb232a61bb27ecaf4ee888bb213
Willem Jan Willemse <w.j.willemse@xs4all.nl>
2021-09-01 09:09:10 +0200

    hyperlink function added

--- nafigator/utils.py
+++ nafigator/utils.py
@@ -9,6 +9,8 @@ from lxml import etree
 import pandas as pd
 import logging
 from nafigator import parse2naf
+import docx
+from docx.enum.dml import MSO_THEME_COLOR_INDEX
 
 
 def dataframe2naf(
@@ -43,7 +45,7 @@ def dataframe2naf(
             output = df_meta.loc[row, "naf:source"]
         else:
             if dc_source is not None:
-                output = os.path.splitext(dc_source)[0]+".naf.xml"
+                output = os.path.splitext(dc_source)[0] + ".naf.xml"
 
         if dc_source and dc_language and output:
 
@@ -64,7 +66,11 @@ def dataframe2naf(
                 continue
             else:
                 # put columns in params
-                params = {col: df_meta.loc[row, col] for col in df_meta.columns if col not in ["naf:source", "naf:status"]}
+                params = {
+                    col: df_meta.loc[row, col]
+                    for col in df_meta.columns
+                    if col not in ["naf:source", "naf:status"]
+                }
                 try:
                     doc = parse2naf.generate_naf(
                         input=dc_source,
@@ -187,3 +193,39 @@ def remove_control_characters(html):
     )
     html = ILLEGAL_XML_CHARS_RE.sub("", html)
     return html
+
+
+def add_hyperlink(paragraph, text, url):
+    # This gets access to the document.xml.rels file and gets a new relation id value
+    part = paragraph.part
+    r_id = part.relate_to(
+        url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True
+    )
+
+    # Create the w:hyperlink tag and add needed values
+    hyperlink = docx.oxml.shared.OxmlElement("w:hyperlink")
+    hyperlink.set(
+        docx.oxml.shared.qn("r:id"),
+        r_id,
+    )
+
+    # Create a w:r element and a new w:rPr element
+    new_run = docx.oxml.shared.OxmlElement("w:r")
+    rPr = docx.oxml.shared.OxmlElement("w:rPr")
+
+    # Join all the xml elements together add add the required text to the w:r element
+    new_run.append(rPr)
+    new_run.text = text
+    hyperlink.append(new_run)
+
+    # Create a new Run object and add the hyperlink into it
+    r = paragraph.add_run()
+    r._r.append(hyperlink)
+
+    # A workaround for the lack of a hyperlink style (doesn't go purple after using the link)
+    # Delete this if using a template that has the hyperlink style in it
+    r.font.color.theme_color = MSO_THEME_COLOR_INDEX.HYPERLINK
+    r.font.size = Pt(8)
+    r.font.underline = True
+
+    return hyperlink
