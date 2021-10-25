9f96ed10592c7341b0bd36a0d33ab8c9238b51a1
Willem Jan Willemse <w.j.willemse@xs4all.nl>
2021-10-25 11:40:11 +0200

    to v0.1.36

--- nafigator/__init__.py
+++ nafigator/__init__.py
@@ -1,6 +1,6 @@
 """Top-level package for nafigator."""
 
-__version__ = "0.1.34"
+__version__ = "0.1.36"
 
 from .cli import *
 from .const import *
--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/denederlandschebank/nafigator',
-    version='0.1.34',
+    version='0.1.36',
     zip_safe=False,
 )
