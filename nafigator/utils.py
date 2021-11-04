f2257dc2f866f28196b9f7f54023dcf095a52baa
Wessel Reijngoud <w.reijngoud@dnb.nl>
2021-11-04 10:00:19 +0100

    moved add_hyperlink

--- nafigator/utils.py
+++ nafigator/utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-"""Utils module.
+"""
+Utils module.
 
 This module contains utility functions for nafigator package
 
@@ -13,11 +14,8 @@ from lxml import etree
 import pandas as pd
 import logging
 from nafigator import parse2naf
-import docx
-from docx.enum.dml import MSO_THEME_COLOR_INDEX
 import datetime
 from typing import Union
-from docx.shared import Pt
 
 def dataframe2naf(
     df_meta: pd.DataFrame,
@@ -441,44 +439,6 @@ def lemmatize_sentence(sentence: dict, terms: dict):
 
     """
     return [terms[term["id"]]["lemma"] for term in sentence["terms"]]
-
-
-def add_hyperlink(paragraph, text, url):
-    # This gets access to the document.xml.rels file and gets a new relation
-    # id value
-    part = paragraph.part
-    r_id = part.relate_to(
-        url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True
-    )
-
-    # Create the w:hyperlink tag and add needed values
-    hyperlink = docx.oxml.shared.OxmlElement("w:hyperlink")
-    hyperlink.set(
-        docx.oxml.shared.qn("r:id"),
-        r_id,
-    )
-
-    # Create a w:r element and a new w:rPr element
-    new_run = docx.oxml.shared.OxmlElement("w:r")
-    rPr = docx.oxml.shared.OxmlElement("w:rPr")
-
-    # Join all the xml elements together add add the required text to the w:r
-    # element
-    new_run.append(rPr)
-    new_run.text = text
-    hyperlink.append(new_run)
-
-    # Create a new Run object and add the hyperlink into it
-    r = paragraph.add_run()
-    r._r.append(hyperlink)
-
-    # A workaround for the lack of a hyperlink style (doesn't go purple after using the link)
-    # Delete this if using a template that has the hyperlink style in it
-    r.font.color.theme_color = MSO_THEME_COLOR_INDEX.HYPERLINK
-    r.font.size = Pt(8)
-    r.font.underline = True
-
-    return hyperlink
 
 def get_terms(pattern, doc):
     """
