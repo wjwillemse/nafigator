6727869ddd44a1ede7bde72a4102fb32e4d3ce11
Hyo Mee Duerinck <h.m.duerinck@dnb.nl>
2022-11-11 11:28:23 +0100

    version bump 0.1.52 in nafigator/__init__.py and setup.py + update HISTORY.rst

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/denederlandschebank/nafigator',
-    version='0.1.51',
+    version='0.1.52',
     zip_safe=False,
 )
